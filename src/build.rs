cb120d49f663594e81fb2c5c9b73dab57f7d6107
Alex Yang <aleozlx@gmail.com>
2018-11-30 23:20:11 -0600

    ..

--- src/build.rs
+++ src/build.rs
@@ -1,18 +1,4 @@
 extern crate cc;
-
 fn main() {
-    cc::Build::new()
-        .file("src/su-exec.c")
-        .compile("suexec");
+    cc::Build::new().file("src/su-exec.c").compile("suexec");
 }
-
-// fn main() {
-//     let out_dir = "target";
-//     Command::new("gcc").args(&["-c", "-o", &format!("{}/su-exec.o", out_dir), "src/su-exec.c"])
-//         .status().unwrap();
-//     Command::new("ar").args(&["rcs", "libsuexec.a", "su-exec.o"])
-//         .current_dir(&Path::new(&out_dir))
-//         .status().unwrap();
-//     println!("cargo:rustc-link-search=native={}", out_dir);
-//     println!("cargo:rustc-link-lib=static=suexec");
-// }
--- src/lib.rs
+++ src/lib.rs
@@ -1,2 +1,7 @@
-extern "C" { fn _su(user: *mut std::os::raw::c_char) -> i32; }
-pub fn su(user: &str) { unsafe { _su(std::ffi::CString::new(user).unwrap().into_raw()); } }
+extern "C" {
+    fn _su(user: *mut std::os::raw::c_char) -> i32;
+}
+
+pub fn su(user: &str) {
+    unsafe { _su(std::ffi::CString::new(user).unwrap().into_raw()); }
+}
