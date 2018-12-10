82c41994c5f13d4e7d06cd8bfc8ef3837e5be963
Alex Yang <aleozlx@gmail.com>
2018-12-10 00:55:46 -0600

    ..

--- src/lib.rs
+++ src/lib.rs
@@ -53,8 +53,8 @@ impl User {
             Some(User {
                 user_name: caps.get(2).unwrap().as_str().to_owned(),
                 group_name: caps.get(4).unwrap().as_str().to_owned(),
-                uid: nix::unistd::Uid::from_raw(caps.get(2).unwrap().as_str().parse().unwrap()),
-                gid: nix::unistd::Gid::from_raw(caps.get(2).unwrap().as_str().parse().unwrap())
+                uid: nix::unistd::Uid::from_raw(caps.get(2).unwrap().as_str().parse().expect("Failed to parse uid")),
+                gid: nix::unistd::Gid::from_raw(caps.get(2).unwrap().as_str().parse().expect("Failed to parse gid"))
             })
         }
         else { None }
@@ -62,17 +62,48 @@ impl User {
 
     pub fn su(&self) -> Result<(), Error> {
         if self.uid.as_raw() != ROOT_UID {
-            match mkuser(&self) {
-                Ok(()) => {
-                    let user_name: &str = &self.user_name;
-                    let ret_su = unsafe { _su(std::ffi::CString::new(user_name).unwrap().into_raw()) };
-                    if ret_su == 0 { Ok(()) }
-                    else { Err(Error::NixError(nix::Error::last())) }
-                }
-                Err(e) => Err(Error::IOError(e))
-            }
+            let _ = self.gen_etc_passwd();
+            let _ = self.gen_etc_group();
+            let _ = self.gen_etc_sudoer();
+            let _ = self.gen_home_dir();
+            let user_name: &str = &self.user_name;
+            let ret_su = unsafe { _su(std::ffi::CString::new(user_name).unwrap().into_raw()) };
+            if ret_su == 0 { Ok(()) }
+            else { Err(Error::NixError(nix::Error::last())) }
         }
         else { Ok(()) }
+    }
+
+    fn gen_etc_passwd(&self) -> Result<(), std::io::Error> {
+        // TODO check existence
+        let mut etc_passwd = OpenOptions::new().append(true).open("/etc/passwd")?;
+        writeln!(etc_passwd, "{}", self.etc_passwd())?;
+        Ok(())
+    }
+
+    fn gen_etc_group(&self) -> Result<(), std::io::Error> {
+        // TODO check existence
+        let mut etc_group = OpenOptions::new().append(true).open("/etc/group")?;
+        writeln!(etc_group, "{}", self.etc_group())?;
+        Ok(())
+    }
+
+    fn gen_etc_sudoer(&self) -> Result<(), std::io::Error> {
+        // TODO check existence
+        let ref fsudoer = format!("/etc/sudoers.d/${username}", username = &self.user_name);
+        let mut etc_group = OpenOptions::new().write(true).truncate(true).open(fsudoer)?;
+        writeln!(etc_group, "{}", self.etc_sudoer())?;
+        let mut permissions = etc_group.metadata()?.permissions();
+        permissions.set_mode(0o440);
+        Ok(())
+    }
+
+    fn gen_home_dir(&self) -> Result<(), Error> {
+        let home: &str = &self.home_dir();
+        if !Path::new(home).exists() { std::fs::create_dir(home)?; }
+        // TODO ways to avoid chown?
+        nix::unistd::chown(home, Some(self.uid), Some(self.gid))?;
+        Ok(())
     }
 }
 
@@ -90,31 +121,14 @@ impl std::fmt::Display for Error {
     }
 }
 
-fn mkuser(user: &User) -> Result<(), std::io::Error> {
-    {
-        let mut etc_passwd = OpenOptions::new().append(true).open("/etc/passwd")?;
-        writeln!(etc_passwd, "{}", user.etc_passwd())?;
+impl From<std::io::Error> for Error {
+    fn from(e: std::io::Error) -> Self {
+        Error::IOError(e)
     }
-    {
-        let mut etc_group = OpenOptions::new().append(true).open("/etc/group")?;
-        writeln!(etc_group, "{}", user.etc_group())?;
-    }
-    {
-        let ref fsudoer = format!("/etc/sudoers.d/${username}", username = &user.user_name);
-        let mut etc_group = OpenOptions::new().write(true).truncate(true).open(fsudoer)?;
-        writeln!(etc_group, "{}", user.etc_sudoer())?;
-        let mut permissions = etc_group.metadata().unwrap().permissions();
-        permissions.set_mode(0o440);
-    }
-    {
-        let home: &str = &user.home_dir();
-        if !Path::new(home).exists() {
-            std::fs::create_dir(home)?;
-        }
-        match nix::unistd::chown(home, Some(user.uid), Some(user.gid)) {
-            Ok(()) => Ok(()),
-            Err(e) => Err(std::io::Error::new(std::io::ErrorKind::Other, format!("{}", e)))
-        }
+}
 
+impl From<nix::Error> for Error {
+    fn from(e: nix::Error) -> Self {
+        Error::NixError(e)
     }
 }
